43edfab57c90b23f740244f83c91943a830e7377
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-14 15:44:08 -0500

    Updated bar type and layout
    Working base commit

--- src/DashDataVisualization.py
+++ src/DashDataVisualization.py
@@ -8,31 +8,28 @@ import dash_html_components as dhtml
 import plotly
 import plotly.graph_objs as pgo
 
+from collections import deque
+
 class DashVisualizer():
 	def __init__(self, pi_data):
 		self.app = dash.Dash(__name__)
 		self.app.layout = dhtml.Div([
 				dcc.Graph(id='live-pi-graph', animate=True),
-				dcc.Interval(id='update-graph', interval=2000)
-		])
-		
+				dcc.Interval(id='update-graph', interval=300)])
 		self.ix = 0
 		self.pi_data = pi_data[2:] #Skip '3.'
 		self.data_length = len(self.pi_data)
-		self.x_axis = [str(label) for label in range(0, 9)]
-		self.width = [.08 for width in range (0, 9)]
-		self.y_axis = [0] * 10
-		
+		self.y_axis = deque(maxlen=150)
+		self.x_axis = deque(maxlen=150)
 		@self.app.callback(Output('live-pi-graph', 'figure'), 
 							events=[Event('update-graph', 'interval')])
 		def updateBarGraph():
-			print(self.ix)
 			if self.ix < self.data_length:
-				self.y_axis[int(self.pi_data[self.ix])] +=1 # Count the number of occurences of a digit
-				print(self.y_axis[int(self.pi_data[self.ix])])
+				self.x_axis.append(self.ix)
+				self.y_axis.append(self.pi_data[self.ix])
 				self.ix +=1
-				data = pgo.Bar(x=self.x_axis, y=self.y_axis, width=self.width, visible=True, name="Digits of Pi")
-				
-				return {'data': [data], 'layout': pgo.Layout(xaxis=dict(range=[0, 9]), yaxis=dict(range=[0, max(self.y_axis)]))}
+				data = pgo.Scatter(x=list(self.x_axis), y=list(self.y_axis), name="Digits of Pi",
+				mode = 'lines+markers', marker=dict(size=10, color='rgba(255, 0, 0, .9)', line=dict(width=2)))
+				return {'data': [data], 'layout': pgo.Layout(title='Pi Day 2018 by @swoldemi', xaxis=dict(range=[min(self.x_axis), max(self.x_axis)]), yaxis=dict(range=[0, 12]), autosize=True)}
 			else:
 				sys.exit(0)
