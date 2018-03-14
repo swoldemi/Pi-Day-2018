030e82c087925fe9d1473b241b216ae27a8ac5d4
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-14 01:13:56 -0500

    Move important declarations
    Load digits in a function
    
    Another deploy attempt

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 from DashDataVisualization import DashVisualizer
 # Visualizing the first billion digits of Pi using Dash (for Pi Day 2018!)
-with open('../billion-pi-digits.txt', 'rt') as pi_digits:
-	digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
-	pi_digits.close()
 
-	dasher = DashVisualizer(digits)
-	server = dasher.app.server
-	app = dasher.app
+def getData():
+	with open('../billion-pi-digits.txt', 'rt') as pi_digits:
+		digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
+		pi_digits.close()
+	return digits
+
 
 if __name__ == '__main__':
+	dasher = DashVisualizer(getData())
+	server = dasher.app.server
+	app = dasher.app
 	app.run_server(debug=True)
