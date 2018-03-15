b602a415b1a230c16d97c96904c7dc23652bf18b
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-14 19:56:52 -0500

    Add server var for deploy

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from DashDataVisualization import DashVisualizer
 # Visualizing the first billion digits of Pi using Dash (for Pi Day 2018!)
+
 def main():
 	with open('../billion-pi-digits.txt', 'rt') as pi_digits:
 		digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
 		pi_digits.close()
 
 	dasher = DashVisualizer(digits)
+	server = dasher.app.server
 	dasher.app.run_server(debug=True)
 	
 if __name__ == '__main__':
