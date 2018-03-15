08a959ac59be140a5b0db981081e75a96cd3ad95
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-15 09:26:34 -0500

    Updated for another deploy attempt

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from DashDataVisualization import DashVisualizer
 # Visualizing the first billion digits of Pi using Dash (for Pi Day 2018!)
-	
-if __name__ == '__main__':
-	with open('../billion-pi-digits.txt', 'rt') as pi_digits:
-		digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
-		pi_digits.close()
+with open('../billion-pi-digits.txt', 'rt') as pi_digits:
+	digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
+	pi_digits.close()
 
 	dasher = DashVisualizer(digits)
 	server = dasher.app.server
 	app = dasher.app
+
+if __name__ == '__main__':
 	app.run_server(debug=True)
