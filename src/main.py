0987bd7d49c4808bf0fa231d6c0e396e0dd85d38
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-03-14 20:26:46 -0500

    Update server position for guincorn deploy

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 from DashDataVisualization import DashVisualizer
 # Visualizing the first billion digits of Pi using Dash (for Pi Day 2018!)
-
-def main():
+	
+if __name__ == '__main__':
 	with open('../billion-pi-digits.txt', 'rt') as pi_digits:
 		digits = list(str(pi_digits.readlines()).replace('[', '').replace(']','').strip("'"))
 		pi_digits.close()
 
 	dasher = DashVisualizer(digits)
+	server = dasher.app.server
 	dasher.app.run_server(debug=True)
-	return dasher.app.server
-	
-if __name__ == '__main__':
-	server = main()
